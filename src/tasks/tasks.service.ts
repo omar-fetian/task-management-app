98b5ef65487686b150f5476e4b2ee6c19162da0d
Omar Fetian <omarfetian_148@outlook.com>
2025-11-24 03:21:57 +0200

    creating a Task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task } from './task.model';
 @Injectable()
 export class TasksService {
-  private tasks = ['Task 1', 'Task 2', 'Task 3'];
+  private tasks: Task[] = [];
 
   getAllTasks() {
     return this.tasks;
