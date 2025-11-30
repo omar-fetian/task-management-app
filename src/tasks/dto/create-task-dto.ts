b117df5e13d01605636f1766e3c761fcd164699e
Omar Fetian <omarfetian_148@outlook.com>
2025-11-30 02:08:34 +0200

    adding pipes validation level

--- src/tasks/dto/create-task-dto.ts
+++ src/tasks/dto/create-task-dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty()
   title: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty()
   description: string;
 }
